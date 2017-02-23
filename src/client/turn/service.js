ac43592f36e1a85dfa59a8bf7f8cedf402bcee10
Greg <gregmichaeldavies@gmail.com>
2017-02-23 16:39:42 +0000

    Update service.js

--- src/client/turn/service.js
+++ src/client/turn/service.js
@@ -7,12 +7,15 @@ Each procreation/death roll should be logged.
 */
 
 angular.module('turn').service(function (organismService) {
+  function forEachOrganism(fnc) {
+    organismService.all().forEach(fnc);
+  }
   function procreate(random) {
     function adjacent(organism) {
       var environments = environmentService.adjacents(organism.location());
       return environments[Math.floor(random * environments.length)];
     }
-    organismService.all().forEach(function (organism) {
+    forEachOrganism(function (organism) {
       var environment = adjacent(Math.random());
       var probability = 1 - organism.deviation();
       if (Math.random() < probability) {
@@ -20,6 +23,15 @@ angular.module('turn').service(function (organismService) {
       }
     });
   }
+  function die() {
+    forEachOrganism(function (organism) {
+      if (Math.random() < organism.deviation()) {
+        organism.die();
+      }
+    });
+    organismService.clean();
+  }
+  // If organism count === 0, simulation ends.
   function next() {
     procreate();
     die();
