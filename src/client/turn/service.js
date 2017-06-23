eb3f2a9ef6a557a3480807806626bdb0a4403f67
Greg <gregmichaeldavies@gmail.com>
2017-06-23 11:25:26 +0100

    Update service.js

--- src/client/turn/service.js
+++ src/client/turn/service.js
@@ -1,33 +1,61 @@
-angular.module('turn').service(function (organismService) {
-  function forEachOrganism(fnc) {
-    organismService.all().forEach(fnc);
+import organismService from '../organism/service'
+import environmentService from '../environment/service'
+
+const config = {
+  timeout: 50
+};
+
+const status = {
+  turns: 0
+};
+
+function forEachOrganism(fnc) {
+  organismService.all().forEach(fnc);
+}
+
+// 
+function procreate(random) {
+  function adjacent(organism) {
+    const environments = environmentService.adjacents(organism.location());
+    return environments[Math.floor(random * environments.length)];
   }
-  function procreate(random) {
-    function adjacent(organism) {
-      var environments = environmentService.adjacents(organism.location());
-      return environments[Math.floor(random * environments.length)];
+  forEachOrganism(function (organism) {
+    const environment = adjacent(Math.random());
+    const probability = 1 - organism.deviation();
+    if (Math.random() < probability) {
+      organism.procreate(environment);
     }
-    forEachOrganism(function (organism) {
-      var environment = adjacent(Math.random());
-      var probability = 1 - organism.deviation();
-      if (Math.random() < probability) {
-        organism.procreate(environment);
-      }
-    });
-  }
-  function die() {
-    forEachOrganism(function (organism) {
-      if (Math.random() < organism.deviation()) {
-        organism.die();
-      }
-    });
-    organismService.clean();
-  }
-  // If organism count === 0, simulation ends.
-  function next() {
-    procreate();
-    die();
-  }
+  });
+}
+
+// Kills off all unlucky organisms.
+function die() {
+  forEachOrganism((organism) => {
+    if (Math.random() > organism.deviation()) {
+      organism.die();
+    }
+  });
+  organismService.clean();
+}
+
+// Start/continue the simulation.
+function start() {
+  // Run the appropriate simulation steps.
+  procreate();
+  die();
   
-  this.next = next;
-});
+  // Update the status object.
+  status.turns += 1;
+  status.extinction = organismService.all().length === 0;
+  
+  // If extinction, pause.
+  if (!status.extinction) {
+    setTimeout(start, config.timeout);
+  }
+}
+
+module.exports = {
+  start, // Run this function to start/continue the simulation.
+  config, // Adjust the parameters of the simulation with the config object.
+  status // Regard the status of the simulation with the status object.
+};
