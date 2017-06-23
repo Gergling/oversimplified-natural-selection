9e032bb484f097fb0787dedb07e5b51063badf1b
Greg <gregmichaeldavies@gmail.com>
2017-06-23 12:14:21 +0100

    Update factory.js

--- src/client/environment/factory.js
+++ src/client/environment/factory.js
@@ -1,23 +1,8 @@
-angular.module('environment').factory(function (attributeFactory) {
-  function Environment() {
-    var data = {
-      attributes: attributeFactory()
-    };
-    
-    function attributes(attributesInstance) {
-      if (attributesInstance !== undefined) {
-        data.attributes.set(attributesInstance);
-      }
-      return data.attributes;
-    }
-    
-    this.attributes = attributes;
+class Environment {
+  constructor(location, attribute) {
+    this.location = location;
+    this.attribute = attribute;
   }
-  function instantiate(location, attributes) {
-    var environment = new Environment();
-    return environment
-      .location(location)
-      .attributes(attributes);
-  }
-  return instantiate;
-});
+}
+
+module.exports = (location, attributes) => new Environment(location, attributes);
