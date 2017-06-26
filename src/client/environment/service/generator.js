d512a40282966a0a22b26bb02e0cb0de4a3a4fee
Greg <gregmichaeldavies@gmail.com>
2017-06-26 13:33:03 +0100

    Update generator.js

--- src/client/environment/service/generator.js
+++ src/client/environment/service/generator.js
@@ -4,6 +4,7 @@ class Painter {
   constructor(width, height, map) {
     // width and height limitations
     // map is an array of environments
+    // populate map with black environments or something
   }
   single(x, y, attribute) {
     // sets a specific location to a copy of the given attribute
@@ -19,6 +20,19 @@ class Painter {
   }
   stripe(x, y, gradient, attribute) {
     // Paints a stripe across the environment.
+  }
+  rectangle(x1, y1, x2, y2, attribute) {
+    // Paints a rectangle
+  }
+  background(attribute) {
+    // Paints all the environments to the same attribute.
+    // rectangle(0, 0, this.width, this.height, attribute)
+  }
+  random() {
+    // Generates a completely random environment.
+    // start with a background
+    // then switch to any of the random functions above, from stripe, rectangle, circle or single
+    // maybe this requires its own object
   }
 }
 
